0f091003f43ef81f4cc1de11c7be94e7ead0a17a
Tiago Justino <tiago.vmj@gmail.com>
2015-09-07 11:56:17 -0700

    preparing data to plot.

--- assignment01.2/js/index.js
+++ assignment01.2/js/index.js
@@ -1,53 +1,22 @@
-var GITHUB_URL = 'https://api.github.com/events'
-var X_START = 5;
-var Y_START = 500;
-var X_STEP = 10;
-var Y_STEP = X_STEP;
-var X_LENGTH = 500;
-var Y_LENGTH = X_LENGTH;
-
-var drawLine = function(start, length) {
-  console.log('drawing line from', start, 'to', start + length);
-  var path = new Path();
-  path.strokeColor = 'black';
-  path.moveTo(start);
-  path.lineTo(start + length);
-}
-
-
-var drawPoint = function(point) {
-  var shape = new Shape.Circle(point, 3);
-  shape.strokeColor = 'black';
-  shape.fillColor = 'black';
-}
-
-// got from http://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
-var formateDate = function(timestamp) {
-  var date = new Date(timestamp*1000);
-  var year = date.getFullYear();
-  var month = ('0' + date.getMonth()).substr(-2);
-  var day = ('0' + date.getDate()).substr(-2);
-  var hours = date.getHours();
-  var minutes = ('0' + date.getMinutes()).substr(-2);
-  var seconds = ('0' + date.getSeconds()).substr(-2);
-
-  var formattedTime = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
-
-  return formattedTime;
-}
-
-var drawGraphAxis = function() {
-  drawLine(new Point(0, Y_START), new Size(X_LENGTH + X_STEP, 0));
-  drawLine(new Point(X_STEP, 0), new Size(0, Y_LENGTH + Y_STEP));
-}
+var GITHUB_URL = 'https://api.github.com/events';
+var receivedEvents = {};
+var usersData = [];
+var eventsData = [];
+var collectedData = [];
+var accumulatedData = {};
+var now;
+var startTime;
 
 // modified from http://stackoverflow.com/questions/1584370/how-to-merge-two-arrays-in-javascript-and-de-duplicate-items
-var arrayUnique = function(array) {
+var arrayUnique = function (array) {
+  var i;
+  var j;
   var a = array.concat();
-  for(var i=0; i<a.length; ++i) {
-    for(var j=i+1; j<a.length; ++j) {
-      if(a[i].id == a[j].id)
+  for (i=0; i<a.length; ++i) {
+    for (j=i+1; j<a.length; ++j) {
+      if (a[i].id == a[j].id) {
         a.splice(j--, 1);
+      }
     }
   }
   return a;
@@ -65,10 +34,6 @@ var formatString = function() {
   });
 };
 
-drawGraphAxis();
-
-var collectedData = [];
-
 var populateUsersList = function(data) {
   var compiledObj = {};
   var compiledData = [];
@@ -76,11 +41,11 @@ var populateUsersList = function(data) {
     var login = data[i].actor.login;
     var type = data[i].type.substr(0, data[i].type.indexOf('Event'))
     if(compiledObj[login]) {
-      compiledObj[login].events += 1;
+      compiledObj[login].n += 1;
       compiledObj[login].lastEventType = type;
     } else {
       compiledObj[login] = {
-        events: 1,
+        n: 1,
         lastEventType: type,
         login: login,
         avatar: data[i].actor.avatar_url
@@ -90,15 +55,15 @@ var populateUsersList = function(data) {
   for(var o in compiledObj) {
     compiledData.push(compiledObj[o]);
   }
-  var sortedData = compiledData.sort(function(a, b) {
-    return a.events - b.events;
+  usersData = compiledData.sort(function(a, b) {
+    return a.n - b.n;
   });
   $('#userslist').empty();
   for(var i = 1; i <= 5; i++) {
-    var user = sortedData[sortedData.length - i];
+    var user = usersData[usersData.length - i];
     if(user) {
       var str = '<li><img style="max-height: 100px; max-width: 100px" src="{0}"> {1} has {2} events, with the last being a {3}.</li>';
-      var formatted = formatString(str, user.avatar, user.login, user.events, user.lastEventType)
+      var formatted = formatString(str, user.avatar, user.login, user.n, user.lastEventType)
       $('#userslist').append(formatted);
     }
   }
@@ -118,16 +83,16 @@ var populateEventsList = function(data) {
   for(var o in compiledObj) {
     compiledData.push(compiledObj[o]);
   }
-  var sortedData = compiledData.sort(function(a, b) {
+  eventsData = compiledData.sort(function(a, b) {
     return a.n - b.n;
   });
   $('#eventslist').empty();
-  for(var i = 1; i <= sortedData.length; i++) {
-    var event = sortedData[sortedData.length - i];
+  for(var i = 1; i <= eventsData.length; i++) {
+    var event = eventsData[eventsData.length - i];
     if(event) {
       var str = '<li>{0} -> {1}</li>';
       var formatted = formatString(str, event.type, event.n);
-      $('#userslist').append(formatted);
+      $('#eventslist').append(formatted);
     }
   }
 }
@@ -136,11 +101,76 @@ var requestData = function() {
   $.getJSON( GITHUB_URL, onFetchedData);
 }
 
+var updateAccumulatedData = function() {
+  for(i = 0; i < usersData.length; i++) {
+    if(!accumulatedData[usersData[i].login]) {
+      accumulatedData[usersData[i].login] = [];
+    }
+    accumulatedData[usersData[i].login].push({
+      time: now,
+      value: usersData[i].n
+    })
+  }
+
+  for(i = 0; i < eventsData.length; i++) {
+    if(!accumulatedData[eventsData[i].type]) {
+      accumulatedData[eventsData[i].type] = [];
+    }
+    accumulatedData[eventsData[i].type].push({
+      time: now,
+      value: eventsData[i].n
+    })
+  }
+}
+
+var plotData = function(label, data) {
+  console.log(label, data);
+}
+
+var updateGraph = function() {
+  updateAccumulatedData();
+  if (usersData) {
+    for(var i = 1; i <= 2; i++) {
+      var user = usersData[usersData.length - i];
+      if(user) {
+        plotData(user.login, accumulatedData[user.login]);
+      }
+    }
+  }
+
+  if (eventsData) {
+    for(var i = 1; i <= 2; i++) {
+      var event = eventsData[eventsData.length - i];
+      if(event) {
+        plotData(event.type, accumulatedData[event.type]);
+      }
+    }
+  }
+}
+
 var onFetchedData = function( data ) {
-  collectedData = arrayUnique(collectedData.concat(data));
-  populateUsersList(collectedData);
-  populateEventsList(collectedData);
+  for(var i = 0; i < data.length; i++) {
+    if(receivedEvents[data[i].id]) {
+      data.splice(i, 1);
+      i--;
+    } else {
+      receivedEvents[data[i].id] = true;
+      data[i].now = now;
+    }
+  }
+  if(data.length > 0) {
+    now = getSeconds();
+    collectedData = collectedData.concat(data);
+    populateUsersList(collectedData);
+    populateEventsList(collectedData);
+    updateGraph();
+  }
   setTimeout(requestData, 1000);
 }
 
+var getSeconds = function() {
+  return new Date().getTime() / 1000;
+}
+
+startTime = getSeconds();
 requestData();
