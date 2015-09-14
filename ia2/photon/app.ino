b2b96c4ba5c66912666378cb5a532bac09ca9be2
Tiago Justino <tiago.vmj@gmail.com>
2015-09-14 19:08:18 -0400

    reading accelerometer

--- ia2/photon/app.ino
+++ ia2/photon/app.ino
@@ -1,10 +1,24 @@
+#include "mma8452.h"
+
+MMA8452 accel;
+
 void setup() {
+  accel.begin();
   Serial.begin(9600);
   pinMode(D7, OUTPUT);
 }
 
 void loop() {
-  Serial.println(analogRead(A0));
+  char str[64];
+  int x, y, z, light;
+
+  accel.read(x,y,z);
+  light = analogRead(A0);
+
+  sprintf(str, "%d:%d:%d:%d", x, y, z, light);
+
+  Serial.println(str);
+
   digitalWrite(D7, HIGH);
   delay(1000);
   digitalWrite(D7, LOW);
