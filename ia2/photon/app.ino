06fe282598c165f3e516b73c2c0b3d59079dd444
Tiago Justino <tiago.vmj@gmail.com>
2015-09-17 15:59:06 -0400

    playing nyan cat song.

--- ia2/photon/app.ino
+++ ia2/photon/app.ino
@@ -1,718 +1,18 @@
 #include "MMA8452Q.h"
 #include "pitches.h"
+#include "nyancat.h"
 
 MMA8452Q accel;
-bool nyan = false;
+int ledFrequency = 1000;
+volatile bool nyan = false;
 int eventSoundCounter = 0;
 bool eventSound = false;
 byte pl = LOCKOUT, prevPl = LOCKOUT;
 int buzzerPin = D2;
 int frequencies[] = {2000, 2400, 2800, 3200, 3600, 4000};
-
-/*
- Nyan Cat
- 
- Plays Nyan Cat
- example: http://www.youtube.com/watch?v=eFHfGA5e4n8
- circuit:
- * 8-ohm speaker on digital pin 8
-  
- By: Bruce Helsen (geobruce) 
-     Stan Draulans (purewantfun)
-     
-
-  Adapted by Tiago justino to work alongside with Photon and Accelerometer.
-
-  original source from http://www.instructables.com/id/Nyan-Cat-on-Arduino/
-  */
-
-int introSize = 72;
-int intro[] = {
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_E5,
-  NOTE_E5,
-  0,
-  NOTE_FS5,
-  0,
-  NOTE_B5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_E5,
-  NOTE_E5,
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_B5,
-  NOTE_B5,
-  0,
-  NOTE_CS6,
-  NOTE_CS6,
-  0,
-  NOTE_DS6,
-  NOTE_DS6,
-  0,
-  NOTE_CS6,
-  NOTE_CS6,
-  0,
-  NOTE_AS5,
-  NOTE_AS5,
-  0,
-  NOTE_B5,
-  0,
-  NOTE_FS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_E5,
-  NOTE_E5,
-  0,
-  NOTE_FS5,
-  0,
-  NOTE_B5,
-  0,
-  NOTE_CS6,
-  NOTE_CS6,
-  0,
-  NOTE_AS5,
-  NOTE_AS5,
-  0,
-  NOTE_B5,
-  NOTE_B5,
-  0,
-  NOTE_CS6,
-  NOTE_CS6,
-  0,
-  NOTE_E6,
-  NOTE_E6,
-  0,
-  NOTE_DS6,
-  NOTE_DS6,
-  0,
-  NOTE_E6,
-  NOTE_E6,
-  0,
-  NOTE_B5, 
-  NOTE_B5, 
-  0
-  };
-
-int melodySize = 608;
-  int melody[] = {
-  NOTE_FS5,
-  0,
-  NOTE_GS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  REST,    
-  REST,    
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_D5, 
-  NOTE_D5, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  REST,    
-  REST,    
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  0,
-  NOTE_D5, 
-  0,
-  NOTE_D5, 
-  NOTE_D5, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_GS5,
-  NOTE_GS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_DS5,
-  0,
-  NOTE_FS5,
-  0,
-  NOTE_GS5,
-  NOTE_GS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_D5, 
-  NOTE_D5, 
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_D5, 
-  NOTE_D5, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_D5, 
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  0,
-  NOTE_FS5,
-  0,
-  NOTE_GS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  REST,    
-  REST,    
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_D5, 
-  NOTE_D5, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  REST,    
-  REST,    
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  0,
-  NOTE_D5, 
-  0,
-  NOTE_D5, 
-  NOTE_D5, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_GS5,
-  NOTE_GS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_DS5,
-  0,
-  NOTE_FS5,
-  0,
-  NOTE_GS5,
-  NOTE_GS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_D5, 
-  NOTE_D5, 
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_D5, 
-  NOTE_D5, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_D5, 
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_E5, 
-  NOTE_E5, 
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_E5, 
-  NOTE_E5, 
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_E5, 
-  NOTE_E5, 
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_DS4,
-  NOTE_DS4,
-  0,
-  NOTE_E4, 
-  NOTE_E4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_AS4,
-  NOTE_AS4,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_E4, 
-  NOTE_E4, 
-  0,
-  NOTE_E5, 
-  NOTE_E5, 
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_E5, 
-  NOTE_E5, 
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_AS4,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_E5, 
-  NOTE_E5, 
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_E5, 
-  NOTE_E5, 
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_E5, 
-  NOTE_E5, 
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_DS4,
-  NOTE_DS4,
-  0,
-  NOTE_E4, 
-  NOTE_E4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  NOTE_CS5,
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_B4,   
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_AS4,
-  NOTE_AS4,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_FS4,
-  NOTE_FS4,
-  0,
-  NOTE_GS4,
-  NOTE_GS4,
-  0,
-  NOTE_B4, 
-  NOTE_B4, 
-  0,
-  NOTE_E5, 
-  NOTE_E5, 
-  0,
-  NOTE_DS5,
-  NOTE_DS5,
-  0,
-  NOTE_E5, 
-  NOTE_E5, 
-  0,
-  NOTE_FS5,
-  NOTE_FS5,
-  0,
-  NOTE_B4, 
-  0,
-  NOTE_CS5,
-  0
-  };
+int nextNyanSoundIndex;
+unsigned long nextNyanSoundTime;
+unsigned long now;
 
 int eventHandler(String data) {
   eventSoundCounter = 0;
@@ -779,34 +79,6 @@ void playSong(int frequency) {
   }
 }
 
-void playMelody() {
-  for (int thisNote = 0; thisNote < 216; thisNote++) {
-
-    // to calculate the note duration, take one second 
-    // divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    tone(buzzerPin, melody[thisNote], 150);
-
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
-    // stop the tone playing:
-    
-  }
-}
-
-void playNyanSound() {
-  if(nyanSoundCounter < introSize) {
-    playSong(intro[nyanSoundCounter]);
-  } else {
-    playSong(melody[nyanSoundCounter - introSize]);
-  }
-  if (nyanSoundCounter == introSize + melodySize) {
-    nyanSoundCounter = 0;
-  } else {
-    nyanSoundCounter++;
-  }
-}
-
 int eventMelodySize = 13;
 int eventMelody[] = {NOTE_C7, NOTE_CS7, NOTE_D7, NOTE_DS7, NOTE_E7, NOTE_F7, NOTE_FS7, NOTE_G7, NOTE_GS7, NOTE_A7, NOTE_AS7, NOTE_B7, NOTE_C8};
 
@@ -821,51 +93,91 @@ void playEventSound() {
   }
 }
 
-void loop() {
+void sendData() {
   char str[64];
   int light;
-  int tap;
   String orientation;
 
-  if(counter == 1000) {
-    accel.read();
-    light = analogRead(A0);
-    prevPl = pl;
-    pl = accel.readPL();
-    orientation = getOrientation(pl);
-    tap = accel.readTap();
+  light = analogRead(A0);
+  pl = accel.readPL();
+  orientation = getOrientation(pl);
 
-    if(prevPl != pl && !nyan) {
+  sprintf(str, "%d:%d:%d:%d:%s:%d", accel.x, accel.y, accel.z, light, orientation.c_str(), nyan);
+  Serial.println(str);
+  Spark.publish("accelData", str);
+}
+
+void readAccel() {
+  int tap;
+
+  accel.read();
+  tap = accel.readTap();
+
+  if(tap > 0) {
+    nyan = !nyan;
+    if(nyan) {
+      ledFrequency = 250;
+      nextNyanSoundIndex = 0;
+      nextNyanSoundTime = now - 1;
+    } else {
+      ledFrequency = 1000;
+    }
+  }
+}
+
+void checkEventSound() {
+}
+
+void checkNyanSound() {
+  if(now > nextNyanSoundTime) {
+    int thisNote = nyanMelody[nextNyanSoundIndex];
+    int noteDuration = 1000 / nyanNoteDurations[nextNyanSoundIndex];
+    int pauseBetweenNotes = noteDuration * 1.30;
+    tone(buzzerPin, thisNote, noteDuration);
+    nextNyanSoundIndex += 1;
+    nextNyanSoundTime += noteDuration + pauseBetweenNotes;
+  }
+}
+
+void checkSound() {
+  if(eventSound) {
+    checkEventSound();
+  } else {
+    if (nyan) {
+      checkNyanSound();
+    }
+  }
+}
+
+void toggleLed() {
+  digitalWrite(D7, !digitalRead(D7));
+}
+
+void checkOrientation() {
+  if(prevPl != pl) {
+    prevPl = pl;
+    if(!eventSound && !nyan) {
       playSound(pl);
     }
+  }
+}
 
-    if(tap > 0) {
-      nyan = !nyan;
-    }
-
-    if(!nyan && !eventSound) {
-      digitalWrite(D7, LOW);
-    }
-
-    if(tap > 0 && nyan) {
-      nyanSoundCounter = 0;
-    }
-
-    sprintf(str, "%d:%d:%d:%d:%s:%d", accel.x, accel.y, accel.z, light, orientation.c_str(), nyan);
-    Serial.println(str);
-    Spark.publish("accelData", str);
-    counter = 0;
+void loop() {
+  now = millis();
+  if(now % 250 == 0) {
+    readAccel();
+    checkOrientation();
   }
 
-  if(eventSound) {
-    playEventSound();
-    digitalWrite(D7, counter % 250 ? HIGH : LOW);
-  }
-  if(nyan && !eventSound) {
-    playNyanSound();
-    digitalWrite(D7, counter % 250 ? HIGH : LOW);
+  if(now % 1000 == 0) {
+    sendData();
   }
 
-  counter += 125;
-  delay(125);
+  if(now % ledFrequency == 0) {
+    toggleLed();
+  }
+
+  if(nyan) {
+    checkSound();
+  }
 }
